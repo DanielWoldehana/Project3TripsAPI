0d5ae840687cd56d264ee7a8fe15a352f9a59334
Devin Mauck <jmauck86@gmail.com>
2019-05-22 17:42:16 -0400

    TripUpdate File

---
+++ react-trip-map/src/TripUpdate.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class TripUpdate extends Component {
+    render() {
+        return (
+            <div>
+
+
+
+
+
+
+
+            </div>
+        );
+    }
+}
+
+export default TripUpdate;
