f457fc37081cdedc4a1c5ac2e906f8de4c6ca969
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-05-23 19:33:33 -0400

    old

--- db/seed.js
+++ db/seed.js
@@ -3,20 +3,9 @@ const TripModel = require("../models/tripsModel");
 const ReviewModel = require("../models/ReviewsModel");
 
 const TripData = require("./allTrips.json");
-const ReviewData = require("./reviews.json");
 
 TripModel.deleteMany({}).then(() => {
-  ReviewModel.deleteMany({}).then(() => {
-    for (let i = 0; i < TripModel.length; i++) {
-      TripModel.create(TripData[i]).then(newTrip => {
-        ReviewModel.create(ReviewData[i]).then(newReview => {
-          newTrip.review.push(newReview._id);
-          newReview.trip.push(newTrip._id);
-          newTrip.save();
-          newReview.save();
-          console.log(newTrip);
-        });
-      });
-    }
+  TripModel.create(TripData).then(newTrip => {
+    console.log(newTrip);
   });
 });
