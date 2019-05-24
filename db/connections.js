147964548995f94799509f22fcf6a686441946f4
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-05-24 10:29:51 -0400

    trying to fix update

--- db/connections.js
+++ db/connections.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.DB_URL);
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true
+  });
 } else {
   mongoose.connect("mongodb://localhost:3001/tripTrackerDB", {
     useNewUrlParser: true
